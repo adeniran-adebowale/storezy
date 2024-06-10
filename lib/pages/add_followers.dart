3fe2a0905c83b075e64d0e0857645460dd48e3f7
Root_David <adeniran.adebowale@yahoo.com>
2024-06-10 14:14:30 +0100

    Updated

--- lib/pages/add_followers.dart
+++ lib/pages/add_followers.dart
@@ -4,13 +4,13 @@ import 'package:storezy/controllers/store_controller.dart';
 import 'package:storezy/widgets/rounded_input.dart';
 
 class AddFollowers extends GetView<StoreController> {
-  int follower=0;
+ String follower='';
   AddFollowers({super.key});
 
   
   @override
   Widget build(BuildContext context) {
-    return GetBuilder(builder: (builder)=> Scaffold(
+    return GetBuilder(builder: (StoreController store)=> Scaffold(
       appBar: AppBar(
         title: Text("Add Followers"),
       ),
@@ -29,13 +29,18 @@ class AddFollowers extends GetView<StoreController> {
                       SizedBox(
                         height: 50,
                         width: 300,
-                        child: RoundedInput(hintText: "Add Follower(Numbers)", onSubmit: (p0) => follower=int.parse(p0),),
+                        child: RoundedInput(hintText: "Add Follower", onSubmit: (p0) => follower=p0 ),
                       ),
                       SizedBox(
                         height: 50,
                       ),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          if(follower!=''){
+                            store.addNewFollower(follower);
+                            store.updateFollowerCount();
+                          }
+                        },
                         child: Text("Add Follower"),
                       ),
                     ],
